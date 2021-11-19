7557b73c1cd80fdfb5e1ffa5a07015d3ae210901
Mikadev1996 <charmikadev@gmail.com>
2021-11-19 08:25:35 +0000

    basic HTML, css layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import './style.css';
 console.log("webpack setup test");
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = "Hello";
+
+
+    return element;
+}
+
+document.body.appendChild(component());
